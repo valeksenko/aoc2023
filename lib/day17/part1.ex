601ebc0bd1f7ae2d42b99a83ab54ee7adb66edbe
Val Aleksenko <v.aleksenko@gmail.com>
2023-12-17 21:33:25 -0500

    d17p1 loop detection (still not working)

--- lib/day17/part1.ex
+++ lib/day17/part1.ex
@@ -29,27 +29,28 @@ defmodule AoC2023.Day17.Part1 do
     max_x = map |> Map.keys() |> Enum.map(&elem(&1, 0)) |> Enum.max()
     max_y = map |> Map.keys() |> Enum.map(&elem(&1, 1)) |> Enum.max()
 
-    finish = fn {{pos, _}, _} -> pos == {max_x, max_y} end
-    cost = fn _, {{pos, _}, _} -> map[pos] end
+    finish = fn {{pos, _}, _, _} -> pos == {max_x, max_y} end
+    cost = fn _, {{pos, _}, _, _} -> map[pos] end
     # we can't estimate, so using it as a Dijkstra's
     estimated_cost = fn _, _ -> 1 end
 
-    next = fn {{pos, dir}, dirs} ->
-      @directions[dir] |> Enum.map(fn d -> {move(pos, d), [d | dirs]} end) |> prune(max_x, max_y)
+    next = fn {{pos, dir}, dirs, visited} ->
+      @directions[dir] |> Enum.map(fn d -> {move(pos, d), [d | dirs] |> Enum.take(4), [{pos, dir} | visited]} end) |> prune(visited, max_x, max_y)
     end
 
-    Astar.astar({next, cost, estimated_cost}, {{{0, 0}, :start}, []}, finish)
-    |> Enum.map(fn {{pos, _}, _} -> map[pos] end)
+    Astar.astar({next, cost, estimated_cost}, {{{0, 0}, :start}, [], []}, finish)
+    |> Enum.map(fn {{pos, _}, _, _} -> map[pos] end)
     |> Enum.sum()
   end
 
   defp move({x, y}, {dx, dy}), do: {{x + dx, y + dy}, {dx, dy}}
 
-  defp prune(next, max_x, max_y) do
+  defp prune(next, visited, max_x, max_y) do
     next
-    |> Enum.filter(fn {{{x, y}, _}, _} -> x in 0..max_x && y in 0..max_y end)
-    |> Enum.reject(fn {_, dirs} ->
-      dirs |> Enum.take(4) |> Enum.frequencies() |> (fn d -> Map.values(d) == [4] end).()
+    |> Enum.filter(fn {{{x, y}, _}, _, _} -> x in 0..max_x && y in 0..max_y end)
+    |> Enum.reject(fn {current, _, _} -> current in visited end)
+    |> Enum.reject(fn {_, dirs, _} ->
+      dirs |> Enum.frequencies() |> (fn d -> Map.values(d) == [4] end).()
     end)
   end
 
